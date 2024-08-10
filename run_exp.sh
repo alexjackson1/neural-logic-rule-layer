d9a83efaaf8cbfa64671d5851c1bb79753b54bbf
Alex Jackson <mail@alexjackson.uk>
2024-08-10 13:25:11 +0100

    Update slurm script

--- run_exp.sh
+++ run_exp.sh
@@ -6,9 +6,12 @@
 #SBATCH --time=01:00:00
 #SBATCH --mem=16G
 #SBATCH --gres=gpu
+#SBATCH --partition=gpu
 
 
 module load cuda
 module load python
+
+source env/bin/activate
 
 python /scratch/prj/formalpaca/nlrl/runner.py
