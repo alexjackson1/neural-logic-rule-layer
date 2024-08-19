ae8315752fe40f677d0f71c96a1d97eff482d4c1
Alex Jackson <mail@alexjackson.uk>
2024-08-19 18:02:47 +0100

    Add error logging

--- run_exp.sh
+++ run_exp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=nlrl
 #SBATCH --output=/scratch/prj/formalpaca/nlrl/logs/%A/%a.out
+#SBATCH --error=/scratch/prj/formalpaca/nlrl/logs/%A/%a.err
 #SBATCH --array=0-2399
 #SBATCH --time=01:00:00
 #SBATCH --mem=16G
--- run_exp_cpu.sh
+++ run_exp_cpu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=nlrl
 #SBATCH --output=/scratch/prj/formalpaca/nlrl/logs/%A/%a.out
+#SBATCH --error=/scratch/prj/formalpaca/nlrl/logs/%A/%a.err
 #SBATCH --array=0-2399
 #SBATCH --time=01:00:00
 #SBATCH --mem=16G
-#SBATCH --gres=cpu
 #SBATCH --partition=cpu,nmes_cpu
 
 
